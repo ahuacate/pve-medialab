6091cda41c32a81bd0b45ec0a7c5170d6969c5be
ahuacate <43461046+ahuacate@users.noreply.github.com>
2019-06-04 13:36:23 +0700

    Update build-vpn-gateway-expressvpn.sh

--- openvpn/build-vpn-gateway-expressvpn.sh
+++ openvpn/build-vpn-gateway-expressvpn.sh
@@ -15,7 +15,7 @@ yum -y install epel-release
 yum -y update
 
 # Install Software
-yum install -y openvpn openssh-server nano wget
+yum install -y openvpn openssh-server nano
 
 # Copy the OpenVPN config files from Github
 cd /etc/openvpn
